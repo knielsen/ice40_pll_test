3e98f67c05c3da3717c7e86157711d420bf72908
Kristian Nielsen <knielsen@knielsen-hq.org>
2016-07-28 22:00:30 +0200

    Outout a 172 MHz clock from two daisy-chained PLLs.
    This is strictly somewhat faster than the max freq. that `icetime` reports
    for the synthesised logic, but it did allow me to measure the frequency on
    the scope to check that the PLL setup is correct.

--- test1.v
+++ test1.v
@@ -1,27 +1,36 @@
 module pllclk (input ext_clock, output pll_clock, input nrst, output lock);
-   wire core_out;
-   wire bypass;
+   wire dummy_out1, dummy_out2, int_clk;
+   wire bypass, lock1;
 
    assign bypass = 1'b0;
 
+   /*
+    Setup a 172 MHz PLL output from the 12 MHz external clock input.
+    The exact 172 MHz clock is achieved by daisy-chaining two PLLs.
+    The first PLL multiplies the input 12MHz to 64.5 MHz, which is then
+    further multiplied by the second PLL up to the 172 MHz.
+    */
    SB_PLL40_CORE #(.FEEDBACK_PATH("SIMPLE"), .PLLOUT_SELECT("GENCLK"),
 		   .DIVR(4'd0), .DIVF(7'd85), .DIVQ(3'd4),
 		   .FILTER_RANGE(3'b001)
-   ) mypll (.REFERENCECLK(ext_clock),
-	    .PLLOUTGLOBAL(pll_clock), .PLLOUTCORE(core_out), .LOCK(lock),
+   ) mypll1 (.REFERENCECLK(ext_clock),
+	    .PLLOUTGLOBAL(dummy_out1), .PLLOUTCORE(int_clk), .LOCK(lock1),
 	    .RESETB(nrst), .BYPASS(bypass));
+
+   SB_PLL40_CORE #(.FEEDBACK_PATH("SIMPLE"), .PLLOUT_SELECT("GENCLK"),
+		   .DIVR(4'd2), .DIVF(7'd31), .DIVQ(3'd2),
+		   .FILTER_RANGE(3'b001)
+   ) mypll2 (.REFERENCECLK(int_clk),
+	    .PLLOUTGLOBAL(pll_clock), .PLLOUTCORE(dummy_out2), .LOCK(lock),
+	    .RESETB(nrst), .BYPASS(bypass));
+
 endmodule	    
 
 module top (
 	input  crystal_clk,
-	output LED0,
-	output LED1,
-	output LED2,
-	output LED3,
-	output LED4,
-	output LED5,
-	output LED6,
-	output LED7
+	output LED0, output LED1, output LED2, output LED3,
+	output LED4, output LED5, output LED6, output LED7,
+	output TESTPIN
 );
 
    localparam BITS = 8;
@@ -44,4 +53,5 @@ module top (
    end
 
    assign {LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7} = outcnt ^ (outcnt >> 1);
+   assign TESTPIN = outcnt[0];
 endmodule
